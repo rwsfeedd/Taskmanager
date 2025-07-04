79cfa1d2594ee2b94b691cb4de3917186137b6ec
Samuel B <sbiebsam@gmail.com>
2025-07-04 13:24:21 +0200

    added Method addTask()

--- src/main/java/com/javafx/terminmanagement/TaskList.java
+++ src/main/java/com/javafx/terminmanagement/TaskList.java
@@ -10,5 +10,18 @@ public class TaskList {
         this.name = name;
     }
 
-    public add
+
+    /**
+     * @param task Aufgabe die zu Liste hinzugefügt werden soll
+     * @return true, wenn die Aufgabe erfolgreich in die Liste eingefügt wurde, sonst false
+     */
+    public boolean addTask(Task task) throws Exception {
+        if (task.isNull()) {// Elemente die null entsprechen werden nicht akzeptiert
+            return false;
+        }
+        if (allTasksList.add(task)) {// Element hinzufügen, falls in List noch nicht vorhanden
+            return true;//true zurückgeben, falls erfolgreich
+        }
+        return false;//false zurückgeben, falls element schon vorhanden
+    }
 }
